de54d49af1ec232e383b52bfc7871969c1b4dec0
Nwafor Ebubechukwu <ebubenwafor20@gmail.com>
2025-11-16 13:46:42 +0100

    Add User entity and repository, and include Spring Security and JWT dependencies

---
+++ complaint-chatbot/src/main/java/dev/ebube/complaintchatbot/entity/User.java
@@ -0,0 +1,39 @@
+package dev.ebube.complaintchatbot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password; // hash
+
+    @Column (nullable = false)
+    private String role = "USER";
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt ;
+
+    private LocalDateTime lastLogin;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
